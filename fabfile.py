4b1126bc53caee4bbd07fc3f298d97ccfaf7be34
Janne Alatalo <janne.alatalo@jamk.fi>
2015-04-02 13:38:17 +0300

    Fix many bugs and syntax errrors

--- fabfile.py
+++ fabfile.py
@@ -13,13 +13,14 @@ def take_snapshot(name = None):
     command = "mongodump -h {0} -d {1} -o {2}"
     host = "{0}:{1}".format(mongo_conf['host'], mongo_conf['port'])
 
-    with fabric.lcd(conf['snapshot_dirs']):
-        fabric.local(command.format(host, mongo_conf['db'], dir_name))
-        h.set_symlink("latest", dir_name)
+    with fabric.context_managers.lcd(conf['snapshots_dir']):
+        fabric.operations.local(command.format(host, mongo_conf['db'], dir_name))
+
+    h.set_symlink("latest", dir_name, conf['snapshots_dir'])
 
 def list_snapshots():
     conf = h.parse_config(CONF_FILE)
-    snapshots = h.list_dirs(conf['snapshot_dirs'])
+    snapshots = h.list_dirs(conf['snapshots_dir'])
     fabric.utils.puts("\n".join(snapshots))
 
 def restore(snapshot = "latest"):
@@ -29,5 +30,5 @@ def restore(snapshot = "latest"):
     command = "mongorestore -h {0} -d {1} {2}"
     host = "{0}:{1}".format(mongo_conf['host'], mongo_conf['port'])
 
-    with fabric.lcd(conf['snapshot_dirs']):
-        fabric.local(command.format(host, mongo_conf['db'], snapshot))
+    with fabric.context_managers.lcd(conf['snapshots_dir']):
+        fabric.operations.local(command.format(host, mongo_conf['db'], snapshot))
--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,7 @@
 # helper functions for fabric script
 import time
 import os
+import yaml
 
 def create_dir(name):
     if not name:
@@ -15,10 +16,11 @@ def parse_config(file_name):
     with open(file_name) as f:
         return yaml.safe_load(f)
 
-def set_symlink(link, target):
-    if os.path.exists(link):
-        os.unlink(link)
-    os.symlink(link, target)
+def set_symlink(link, target, parent_dir = "."):
+    link_path = os.path.join(parent_dir, link)
+    if os.path.exists(link_path):
+        os.unlink(link_path)
+    os.symlink(target, link_path)
 
 def list_dirs(parent):
     return filter(os.path.isdir, [os.path.join(parent, d) for d in os.listdir(parent)])
